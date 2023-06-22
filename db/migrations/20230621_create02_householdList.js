f82af865e98c2244bd7259f28ae9cf6885061f01
tomohiro-98 <tomohiro_kuba@mail.toyota.co.jp>
2023-06-22 11:03:59 +0900

    マイグレーション＆シード完了

---
+++ db/migrations/20230621_create02_householdList.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('householdList', function(table) {
+      table.increments("id").primary();
+      table.string('householdName', 32);
+      table.string('householdTel',16);
+      table.string('householdMail',64);
+      table.integer('householdAge');
+      table.integer('familySize');
+      table.string('roleFlag',8);
+      table.string('block1',32);
+      table.string('block2',32);
+      table.string('block3',32);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('householdList');
+};
