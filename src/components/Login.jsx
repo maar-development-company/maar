ad96a65284b9ac2f6b26fdd2d947c5ae2855243f
kidayuichi <114062635+kidayuichi@users.noreply.github.com>
2023-06-24 15:20:55 +0900

    aa

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect, useRef } from "react";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://maar-front.onrender.com/"
+    ? "https://maar-front.onrender.com"
     : "http://localhost:8080";
 
 export const Login = (props) => {
