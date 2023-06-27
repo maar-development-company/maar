07e152fd0c089c255cb45f8ac65990ffd62a44ea
yyamasakii <yumi_yamasaki@mail.toyota.co.jp>
2023-06-28 00:13:37 +0900

    🩹 ：新規投稿の押下後にクリア＆記事一覧更新のトリガーにnumber設定

--- src/components/Articlelist.jsx
+++ src/components/Articlelist.jsx
@@ -16,45 +16,16 @@ const URL =
 		? "https://maar-front.onrender.com"
 		: "http://localhost:8080";
 
-// const testArticleList = [
-//   {
-//     articleTitleID: 1,
-//     title: "ごみ収集の日",
-//     content:
-//       "今週の火曜日はごみ収集の日です。粗大ゴミは、出さないようにしてください。粗大ゴミは今月末の30日に収集がありますので忘れないようにお願いします。",
-//     readFlag: 0,
-//     readTimestamp: "2023/06/21 15:41",
-//     articleTimestamp: now,
-//   },
-//   {
-//     articleTitleID: 30,
-//     title: "大林町自治区総会の日",
-//     content:
-//       "きたる2023年7月15日は大林町の総会になります。皆様のご意見を集約する日でありますのでご面倒ではございますが、ぜひ大林公民館までお願いいたします。これからの良い町大林をみんなで作っていきましょう。",
-//     readFlag: 0,
-//     readTimestamp: "2023/06/21 16:41",
-//     articleTimestamp: now,
-//   },
-//   {
-//     articleTitleID: 100,
-//     title: "ゲートボールの大会を開催します！ぜひご参加ください！",
-//     content:
-//       "6/29日はゲートボール大会の日です。ゲストでゲートボール日本代表であり、シドニーオリンピックで金メダルをとりました、ボブさんが来てくれます。彼のゲートショットは砂煙が舞い上がるほど強烈なショットだと聞いておりますので、当日はサングラスや保護メガネの準備をお願いいたします。",
-//     readFlag: 1,
-//     readTimestamp: "2023/06/21 17:41",
-//     articleTimestamp: now,
-//   },
-// ];
-
 export const ArticleList = (props) => {
 	const { municipalityId, municipality } = props;
 	const [ArticleList, setArticleList] = useState([]);
 	const [elementsArr, setElementsArr] = useState([]);
 	const [ID, setID] = useState(2);
+	const [number, setNumber] = useState("");
 
 	useEffect(() => {
 		getArticleList();
-	}, [ArticleList]);
+	}, [number]);
 
 	const writeToSessionStorage = (key, value) => {
 		sessionStorage.setItem(key, JSON.stringify(value));
@@ -82,6 +53,7 @@ export const ArticleList = (props) => {
 				throw new Error("Failed to fetch.");
 			}
 			const articleObj = await response.json();
+			setNumber(articleObj.length);
 			console.log(articleObj);
 			if (articleObj.length !== ArticleList.length) {
 				setArticleList(articleObj);
@@ -135,33 +107,5 @@ export const ArticleList = (props) => {
 				);
 			})}
 		</div>
-
-		//     <>
-		//       <div>
-		//         <button
-		//           onClick={getArticleList}
-		//           className="bg-blue-800 hover:bg-blue-700 text-white rounded px-4 py-2 w-56"
-		//         >
-		//           更新
-		//         </button>
-		//       </div>
-		//       <div>
-		//         {ArticleList.map((ele) => {
-		//           console.log("");
-		//           return (
-		//             <Link to="/SingleArticle" state={{ articleInfo: ele }}>
-		//               <section className="m-4 p-4 h-44  border-solid rounded-3xl border-4 border-gray-300 text-center flex flex-row">
-		//                 <div className="w-10/12 text-left">
-		//                   <h2 className="text-3xl">{ele.articleTitle}</h2>
-		//                   <p className="mt-4">{ele.articleTimestamp}</p>
-		//                 </div>
-		//                 {/* <div className="">{ele.readFlag === 0 ? "未読" : "既読"}</div> */}
-		//                 <div className="">{ele.readFlag === "1" ? "既読" : "未読"}</div>
-		//               </section>
-		//             </Link>
-		//           );
-		//         })}
-		//       </div>
-		//     </>
 	);
 };
