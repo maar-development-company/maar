e35aaa3ff24ef6df1ecde91b01adbe712374a8a4
MF0820 <masafumi_fukushima_aa@mail.toyota.co.jp>
2023-06-30 14:17:11 +0900

    AdminAssign

---
+++ src/components/AdminAssign.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://maar-front.onrender.com"
+    : "http://localhost:8080";
+
+export const AdminAssign = () => {
+  const aaa = aaa;
+};
