619cfb16f60ddb35b9e79d70237cdc784cc090d0
yyamasakii <yumi_yamasaki@mail.toyota.co.jp>
2023-07-06 14:40:10 +0900

    💄Loadingに時間制限つけてCSS修正

--- src/components/DisplayImage.jsx
+++ src/components/DisplayImage.jsx
@@ -1,8 +1,8 @@
 // PDF表示をreact-pdfに変更
-import React, { useEffect, useState } from 'react';
-import AWS from 'aws-sdk';
-import { Document, Page, pdfjs } from 'react-pdf';
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import React, { useEffect, useState } from "react";
+import AWS from "aws-sdk";
+import { Document, Page, pdfjs } from "react-pdf";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -13,97 +13,114 @@ AWS.config.update({
 });
 
 export const DisplayImage = (props) => {
-
-  const { articleInfo } = props;
+	const { articleInfo } = props;
 	console.log("これが空だと俺のカード表示", articleInfo);
-  const [imageSource, setImageSource] = useState("");
-  const [isPdf, setIsPdf] = useState(false);
-  const [numPages, setNumPages] = useState(null);
-  const [imageUrl, setImageUrl] = useState(
-    articleInfo ? articleInfo.fileSavePath : "スクリーンショット 2023-06-14 19.14.13.png"
-  );
-  const [scale, setScale] = useState(1);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const [imageSource, setImageSource] = useState("");
+	const [isPdf, setIsPdf] = useState(false);
+	const [numPages, setNumPages] = useState(null);
+	const [imageUrl, setImageUrl] = useState(
+		articleInfo
+			? articleInfo.fileSavePath
+			: "スクリーンショット 2023-06-14 19.14.13.png"
+	);
+	const [scale, setScale] = useState(1);
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const [isLoading, setIsLoading] = useState(true);
+	const [loadingTimeout, setLoadingTimeout] = useState(null);
 
-  const s3 = new AWS.S3();
+	const s3 = new AWS.S3();
 
-  useEffect(() => {
-    const getImageFromS3 = async () => {
-      const params = {
-        Bucket: "article-area",
-        Key: imageUrl,
-      };
-      try {
-        const response = await s3.getObject(params).promise();
-        setIsPdf(response.ContentType === "application/pdf");
-
-        const imageBlob = new Blob([response.Body], { type: response.ContentType });
-        const imageUrl = URL.createObjectURL(imageBlob);
-        setImageSource(imageUrl);
-      } catch (error) {
-        console.error("S3からの画像取得エラー:", error);
-      }
-    };
+	useEffect(() => {
+		const getImageFromS3 = async () => {
+			const params = {
+				Bucket: "article-area",
+				Key: imageUrl,
+			};
+			try {
+				const response = await s3.getObject(params).promise();
+				setIsPdf(response.ContentType === "application/pdf");
 
 				const imageBlob = new Blob([response.Body], {
 					type: response.ContentType,
 				});
 				const imageUrl = URL.createObjectURL(imageBlob);
 				setImageSource(imageUrl);
+				setIsLoading(false);
 			} catch (error) {
 				console.error("S3からの画像取得エラー:", error);
+				setIsLoading(false);
 			}
 		};
-    
+
 		if (imageUrl) {
 			getImageFromS3();
+			setIsLoading(true);
+			setLoadingTimeout(
+				setTimeout(() => {
+					setIsLoading(false);
+				}, 3000)
+			);
 		}
 	}, [imageUrl]);
-  
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth);
-  };
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
+	const handleResize = () => {
+		setWindowWidth(window.innerWidth);
+	};
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+	useEffect(() => {
+		window.addEventListener("resize", handleResize);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
-  return (
+	function onDocumentLoadSuccess({ numPages }) {
+		setNumPages(numPages);
+	}
+
+	useEffect(() => {
+		clearTimeout(loadingTimeout);
+	}, [loadingTimeout]);
+
+	return (
 		<div className="mt-4">
-      {imageSource ? (
-        isPdf ? (
-          <TransformWrapper>
-            <TransformComponent>
-              <div style={{ width: "100%", height: "100%", overflow: "auto" }}>
-                <Document
-                  file={imageSource}
-                  onLoadSuccess={onDocumentLoadSuccess}
-                  loading="Loading PDF..."
-                  error="Error loading PDF"
-                >
-                  {Array.from(new Array(numPages), (el, index) => (
-                    <Page key={`page_${index + 1}`} pageNumber={index + 1} width={windowWidth} scale={0.8} />
-                  ))}
-                </Document>
-              </div>
-            </TransformComponent>
-          </TransformWrapper>
-        ) : (
-          <a href={imageSource} data-lightbox="group">
-            <img src={imageSource} width="300" alt="Uploaded Image" />
-          </a>
-        )
-      ) : (
+			{isLoading ? (
 				<div id="loading-icon"></div>
-      )}
-    </div>
-  );
+			) : imageSource ? (
+				isPdf ? (
+					<TransformWrapper>
+						<TransformComponent>
+							<div style={{ width: "100%", height: "100%", overflow: "auto" }}>
+								<Document
+									file={imageSource}
+									onLoadSuccess={onDocumentLoadSuccess}
+									loading="Loading PDF..."
+									error="Error loading PDF">
+									{Array.from(new Array(numPages), (el, index) => (
+										<Page
+											key={`page_${index + 1}`}
+											pageNumber={index + 1}
+											width={windowWidth}
+											scale={0.8}
+										/>
+									))}
+								</Document>
+							</div>
+						</TransformComponent>
+					</TransformWrapper>
+				) : (
+					<a href={imageSource} data-lightbox="group">
+						<img src={imageSource} width="300" alt="Uploaded Image" />
+					</a>
+				)
+			) : (
+				<div className="">
+					<p className="animate-bounce-top text-gray-700 text-2xl">
+						ファイルが正常に登録されておりません
+					</p>
+				</div>
+			)}
+		</div>
+	);
 };
