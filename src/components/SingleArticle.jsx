0719aaf8add1eda7da5061901993f4d4eb4aa6d9
Kohei-Mori2018 <kohei998morizoo@gmail.com>
2023-07-06 11:55:36 +0900

    fix:時間の修正できた？

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -4,29 +4,29 @@ import dayjs from "dayjs";
 import { DisplayImage } from "./DisplayImage";
 
 export const SingleArticle = () => {
-	const location = useLocation();
-	const { articleInfo } = location.state;
-	console.log("articleInfoの中身", articleInfo);
-	return (
-		<div className="overflow-y-auto fixed top-24 bottom-12 right-0 left-0">
-			<div className="m-4 p-1 h-fit shadow-lg border bg-gray-100 border-gray-300  md:h-fit rounded-3xl text-3xl md:flex md:flex-col">
-				<div className="m-1 p-2 h-fit md:h-fit border-b-2 text-3xl">
-					<h1>{articleInfo.articleTitle}</h1>
-				</div>
-				<div className="m-1 mt-3 p-2 h-fit md:h-fit border-solid rounded-2xl text-3xl" >
-					{articleInfo.articleContent.split("\n").map((text, index) => (
-						<React.Fragment key={index}>
-							{text}
-							<br />
-						</React.Fragment>
-					))}
-					<DisplayImage articleInfo={articleInfo} />
-				</div>
-				<p className="mr-4 text-2xl text-right">
-					配信日時：
-					{dayjs(articleInfo.articleTimestamp).format("YYYY年MM月DD日HH時mm分")}
-				</p>
-			</div>
-		</div>
-	);
+  const location = useLocation();
+  const { articleInfo } = location.state;
+  console.log("articleInfoの中身", articleInfo);
+  return (
+    <div className="overflow-y-auto fixed top-24 bottom-12 right-0 left-0">
+      <div className="m-4 p-1 h-fit shadow-lg border bg-gray-100 border-gray-300  md:h-fit rounded-3xl text-3xl md:flex md:flex-col">
+        <div className="m-1 p-2 h-fit md:h-fit border-b-2 text-3xl">
+          <h1>{articleInfo.articleTitle}</h1>
+        </div>
+        <div className="m-1 mt-3 p-2 h-fit md:h-fit border-solid rounded-2xl text-3xl">
+          {articleInfo.articleContent.split("\n").map((text, index) => (
+            <React.Fragment key={index}>
+              {text}
+              <br />
+            </React.Fragment>
+          ))}
+          <DisplayImage articleInfo={articleInfo} />
+        </div>
+        <p className="mr-4 text-2xl text-right">
+          配信日時：
+          {dayjs(articleInfo.articleTimestamp).format("YYYY年MM月DD日hh時mm分")}
+        </p>
+      </div>
+    </div>
+  );
 };
